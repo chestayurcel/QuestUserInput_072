7dec74396cff40b23f39b27750c750dd9e42d891
Yurcell <chestayurcel@gmail.com>
2025-10-30 12:30:42 +0700

    Menambahkan Text dengan string resource "submit" di dalam Button, dan Menambahkan Divider dengan padding medium dan ketebalan divider_tipis.

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -94,7 +95,15 @@ fun FormDataDiri(modifier: Modifier)
                 alamat = textAlamat
             }
         ) {
-
+            Text(stringResource(R.string.submit))
         }
+
+        Divider(
+            modifer = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_medium
+                )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
     }
 }
