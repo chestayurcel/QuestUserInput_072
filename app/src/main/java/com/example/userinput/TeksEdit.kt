2702bfb9f430441a3bb1e30d3095dcff8f5af822
Yurcell <chestayurcel@gmail.com>
2025-10-30 11:39:12 +0700

    membuat composable FormDataDiri

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,2 +1,18 @@
 package com.example.userinput
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import java.lang.reflect.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier)
+{
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+}
