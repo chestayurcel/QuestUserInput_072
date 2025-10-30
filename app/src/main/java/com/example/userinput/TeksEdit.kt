873ac167689385a1d535a468f917c50696777380
Yurcell <chestayurcel@gmail.com>
2025-10-30 11:49:55 +0700

    Pada form data diri menambahkan column, OutlinedTextField, dan Row

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,10 +1,15 @@
 package com.example.userinput
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import java.lang.reflect.Modifier
 
 @Composable
@@ -22,4 +27,21 @@ fun FormDataDiri(modifier: Modifier)
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
 
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+        Row {
+            gender.forEach { item -> }
+        }
+    }
 }
